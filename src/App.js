46c0863f74d418ca1a8a6e5c5c85820948b11cb2
Nguyen Dang Minh <nguyendangminh49@gmail.com>
2023-07-12 00:13:56 +0700

    update

--- src/App.js
+++ src/App.js
@@ -17,29 +17,29 @@ import Check from "~/components/Check"
 
 function App() {
   return (
-      <Router>
-        <div className="App">
-            <Routes>
-                <Route path ="/" element = {<Layout/>} />
-                <Route path = "/register" element = {<Register/>}/>
-                <Route path = "/author" element = {<Author/>}/>
-                <Route path = "/video" element = {<Video/>}/>
-                <Route path = "/text" element = {<Text/>}/>
-                <Route path = "/profile" element = {<Profile/>}/>
-                <Route path = "/lesson" element = {<Lesson/>}/>
-                <Route path = "/subject" element = {<Subject/>}/>
-                <Route path = "/level" element = {<Level/>}/>
-                <Route path = "/point" element = {<Point/>}/>
-                <Route path = "/word" element = {<Word/>}/>
-                <Route path = "/check" element = {<Check/>}/>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/author" element={<Author />} />
+          <Route path="/video" element={<Video />} />
+          <Route path="/text" element={<Text />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/lesson" element={<Lesson />} />
+          <Route path="/subject" element={<Subject />} />
+          <Route path="/level" element={<Level />} />
+          <Route path="/point" element={<Point />} />
+          <Route path="/word" element={<Word />} />
+          <Route path="/check" element={<Check />} />
 
 
 
-            </Routes>
-        </div>
-      </Router>
-    
-  );  
+        </Routes>
+      </div>
+    </Router>
+
+  );
 }
 
 
